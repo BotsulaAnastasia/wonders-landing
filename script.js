ff7e82d7700cf9567f75d4d3419ba21e29739cf0
Anastasia Botsula <nactya.botsula@gmail.com>
2025-10-07 11:31:13 +0300

    feat: implement dropdown with stations in inputs

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import {Stations as STATIONS} from "./stations.js";
+
 // Implement open/close burger menu
 const burgerIcon = document.querySelector('.burger');
 const headerMenu = document.querySelector('.header-menu');
@@ -127,5 +129,59 @@ function activateElement(el) {
     el.removeAttribute('disabled');
 }
 
-//TODO: добавить автобусные станции в инпуты
+// Implement dropdown with stations in inputs
+const inputsForStations = document.querySelectorAll('.text-input.--stations');
+
+inputsForStations.forEach((input) => {
+    input.addEventListener('focus', (e) => {
+        // Show the dropdown after focus on the input
+        toggleNextSiblingElement(e.target);
+
+        renderDropdownContent(e);
+    });
+
+    // Hidden the dropdown after blur the input
+    input.addEventListener('blur', (e) => toggleNextSiblingElement(e.target));
+
+    input.addEventListener('keyup', debounce(renderDropdownContent, 200));
+})
+
+function renderDropdownContent(e) {
+    const inputValue = e.target.value.toLowerCase().trim();
+    let filteredStations = STATIONS;
+
+    if (inputValue !== '') {
+        filteredStations = STATIONS.filter((station) => station.toLowerCase().includes(inputValue));
+    }
+
+    const dropdown = e.target.nextElementSibling;
+    dropdown.innerHTML = '';
+
+    if (filteredStations.length === 0) {
+        dropdown.innerHTML = `<span class="not-found-span">Station not found</span>`;
+    } else {
+        // fill in the dropdown with filtered stations
+        filteredStations.forEach((station) => createDropdownItems(station, dropdown));
+
+        dropdown.addEventListener('mousedown', handleDropdownClick);
+    }
+}
+
+function createDropdownItems(itemText, dropdownElement) {
+    const listItem = document.createElement('li');
+    listItem.innerText = itemText;
+    dropdownElement.appendChild(listItem);
+}
+
+function handleDropdownClick (e) {
+    if (e.target.tagName === 'LI') {
+        const input = e.currentTarget.previousElementSibling;
+        input.value = e.target.innerText;
+    }
+}
+
+function toggleNextSiblingElement(el) {
+    const nextSibling = el.nextElementSibling;
+    nextSibling.classList.toggle('--hidden');
+}
 //TODO: сделать валидацию формы поиска: проверка корректности выбора станций (чтобы не совпадали), проверка даты (чтобы не была в прошлом)
